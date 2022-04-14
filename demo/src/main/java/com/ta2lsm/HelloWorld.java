f4e50eb5d762ddfb07b4f3897ce8c25b796ba76f
TA2LSM <semihsenol@gmail.com>
2022-04-14 15:27:35 +0300

    first commit

--- demo/src/main/java/com/ta2lsm/HelloWorld.java
+++ demo/src/main/java/com/ta2lsm/HelloWorld.java
@@ -30,7 +30,7 @@ public final class HelloWorld {
      * @param args The arguments of the program.
      */
      public static void main(String[] args) {  
-         
+ 
         //--- PIRIMITIVE DATA TYPES ----------------------------
         // boolean isEligible = false;
         // byte age = 30;                      // JRE tarafından bellek ayrılır ve bırakılır
@@ -57,7 +57,7 @@ public final class HelloWorld {
          * 
          * Printf kullanımına ait birkaç örnek:
          *  */  
-        // System.out.printf("%b%n", null);
+        // System.out.printf("%b%n", null);     // %n ile \n aynı işi görüyor
         // System.out.printf("%B%n", false);
         // System.out.printf("%B%n", 5.3);
         // System.out.printf("%b%n", "random text");  
@@ -76,19 +76,34 @@ public final class HelloWorld {
         // ama import gerekmiyor çünkü otomatik olarak zaten import ediliyor.
         // String tanımlaması, aynı aşağıdaki date örneğindeki gibi referans tanımlama şeklinde yapılmalı fakat
         // klasik yöntem de kabul ediliyor. (kolay yöntem)
-        String message1 = new String("Hello World!");
-        String message2 = " I'm Back...";
-        System.out.printf("Msg#1: %s / Msg#2: %s%n", message1, message2);
+        // String message1 = new String("Hello World!");
+        // String message2 = " I'm \"Back...\"";   // \" ile string içinde " kullanılabildi.
+        // System.out.printf("Msg#1: %s / Msg#2: %s%n", message1, message2);
 
-        String message3 = message1 + message2;  // message3 için alt metotlar artık kullanılabilir.
-        System.out.printf("Msg#3: %s%n", message3);
-        System.out.println(message3.endsWith("..."));   // true verir
-        System.out.println(message3.length());   // uzunluğunu verir
+        // String message3 = message1 + message2;  // message3 için alt metotlar artık kullanılabilir.
+        // System.out.printf("Msg#3: %s%n", message3);
+        // System.out.println(message3.endsWith("..."));   // true verir
+        // System.out.println(message3.length());   // uzunluğunu verir
+        // System.out.println(message3.indexOf("what"));   
+        // // aranan string ya da karakterin indeksini döner (0'dan başlar) Eğer bulunamazsa -1 döner
+        // System.out.println(message3.replace("!", "*"));  
+        // bulunan değerleri (! olan hepsini) yenileri ile değiştirir (*) Değişiklik kalıcı değildir.
+        // işlem sonucunu message3'e kaydetmedik !!! String'ler üzerinde yapılan değişiklikler
+        // HER ZAMAN yeni string'ler döner. Bunları bir başka değişkene kaydetmezsek kaybolurlar.     
 
-        Date dateNow = new Date(); // kodu yazarken bizim tarafımızdan bellek ayrılır, JRE tarafından bırakılır
-        System.out.println(dateNow);
-        Date date = new Date(-86_400_000L); // 1.1.1970'den beri ms olarak geçen zaman. Daha evveli için negatif yazılır
-        System.out.println(date);        
+        // trim, toLowerCase, toUpperCase... vs değişik metotlar da var. Detayları araştırınız... 
+        //System.out.println(message3.trim());
+        // trim baştan ve sondan boşlukları atar. Aradaki boşluklara karışmaz.
+
+        // Kaçınılması gereken karakterler
+        // \n: new line, \t: tab, \
+        // String message4 = "C:\\Windows\\system32";  // tek \ olarak kaydedilir.
+        // System.out.println(message4);
+
+        // Date dateNow = new Date(); // kodu yazarken bizim tarafımızdan bellek ayrılır, JRE tarafından bırakılır
+        // System.out.println(dateNow);
+        // Date date = new Date(-86_400_000L); // 1.1.1970'den beri ms olarak geçen zaman. Daha evveli için negatif yazılır
+        // System.out.println(date);        
 
         // System.out.printf("%tT%n", date);
         // System.out.printf("hours %tH: minutes %tM: seconds %tS%n", date, date, date);
@@ -99,19 +114,63 @@ public final class HelloWorld {
         //--- END OF REFERENCE DATA TYPES ----------------------
 
         // x değişse de y değişmez. (primitive data type)
-        byte x = 1; // bellekte x için alan ayrılır ve içeriği 1 olarak yazılır
-        byte y = x;
-        x = 2;
-        System.out.println(y);
+        // byte x = 1; // bellekte x için alan ayrılır ve içeriği 1 olarak yazılır
+        // byte y = x;
+        // x = 2;
+        // System.out.println(y);
 
-        // C'deki pointer'a benzer durum
-        // point1'deki x değişirse point2'deki x de değişir. (aynı alanı gösterdikleri için)
-        Point point1 = new Point(1, 1);
-        // bir bellek adresinde x ve y değerlerini tutan alan ayrılır. İçerikleri doldurulur.
-        // Ayrıca başka bir alanda point1 olarak yukarıda ayrılan alanın adresi tutulur.
-        // (point1 aslında pointer olarak kullanılıyor)
-        Point point2 = point1; // point2, point1'in de gösterdiği adrese eşitlenir. (reference)
-        point1.x = 2;
-        System.out.println(point2);
+        // // C'deki pointer'a benzer durum (reference data type)
+        // // point1'deki x değişirse point2'deki x de değişir. (aynı alanı gösterdikleri için)
+        // Point point1 = new Point(1, 1);
+        // // bir bellek adresinde x ve y değerlerini tutan alan ayrılır. İçerikleri doldurulur.
+        // // Ayrıca başka bir alanda point1 olarak yukarıda ayrılan alanın adresi tutulur.
+        // // (point1 aslında pointer olarak kullanılıyor)
+        // Point point2 = point1; // point2, point1'in de gösterdiği adrese eşitlenir. (reference)
+        // point1.x = 2;
+        // System.out.println(point2);
+
+        //--- ARRAYS -------------------------------------------
+        byte[] numbers = new byte[5];                   // zero initialized şekilde oluşturulur. Uzunluğu 5'tir.
+        int[] numbers2 = {2, 3, 4, 1, 5};               // içeriği girilerek oluşturulur. Uzunluğu 5 olarak atanır ve değiştirilemez !!!
+        String[] names = new String[3];                 // null olacak şekilde oluşturulur. Uzunluğu 3'tür.
+
+        numbers[1] = 1;
+        //numbers[10] = 5; // exception hatası oluşur. Çünkü hafızada numbers için 10. indeks yok
+
+        System.out.println(numbers);                    // numbers dizisinin adresi basılır
+        System.out.println(Arrays.toString(numbers));   // numbers dizisinin içeriği basılır
+
+        System.out.println(numbers[1]);
+        System.out.println(numbers2[2]);
+
+        System.out.println(numbers2.length);
+        System.out.println(Arrays.toString(numbers2));
+        Arrays.sort(numbers2);
+        System.out.println(Arrays.toString(numbers2));      
+
+        System.out.println(Arrays.toString(names));     // names dizisinin içeriği basılır
+
+        // [rows][columns] >> 2, 5
+        int[][] numbers3 = { 
+            {2, 3, 4, 1, 5},
+            {6, 7, 8, 9, 0}
+        };
+
+        //2, 2, 3
+        int[][][] numbers4 = { 
+            { {2, 3, 5}, 
+              {4, 1, 7}
+            },
+            { {7, 8, 3}, 
+              {0, 9, 5}
+            },
+        };
+
+        numbers4[0][1][2] = 6;  // 7'yi 6 olarak değiştirir
+        System.out.println(Arrays.deepToString(numbers3));  // multi dimentional array basmak için
+        System.out.println(Arrays.deepToString(numbers4)); 
+        //------------------------------------------------------
     }
+
+    //...
 }
