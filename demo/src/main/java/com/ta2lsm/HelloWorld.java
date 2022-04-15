14023f57571f00212d79e18029c47bd2b65d5f7d
TA2LSM <semihsenol@gmail.com>
2022-04-15 14:36:39 +0300

    first commit

--- demo/src/main/java/com/ta2lsm/HelloWorld.java
+++ demo/src/main/java/com/ta2lsm/HelloWorld.java
@@ -13,6 +13,12 @@ package com.ta2lsm;
 // istediğimiz gibi çalışmıyordu.
 import java.util.*;
 import java.awt.*;
+import java.net.SocketTimeoutException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+import javax.naming.spi.DirStateFactory.Result;
   
 /**
  * JAVA uygulamasına ait ana class
@@ -130,45 +136,153 @@ public final class HelloWorld {
         // System.out.println(point2);
 
         //--- ARRAYS -------------------------------------------
-        byte[] numbers = new byte[5];                   // zero initialized şekilde oluşturulur. Uzunluğu 5'tir.
-        int[] numbers2 = {2, 3, 4, 1, 5};               // içeriği girilerek oluşturulur. Uzunluğu 5 olarak atanır ve değiştirilemez !!!
-        String[] names = new String[3];                 // null olacak şekilde oluşturulur. Uzunluğu 3'tür.
+        // byte[] numbers = new byte[5];                   // zero initialized şekilde oluşturulur. Uzunluğu 5'tir.
+        // int[] numbers2 = {2, 3, 4, 1, 5};               // içeriği girilerek oluşturulur. Uzunluğu 5 olarak atanır ve değiştirilemez !!!
+        // String[] names = new String[3];                 // null olacak şekilde oluşturulur. Uzunluğu 3'tür.
 
-        numbers[1] = 1;
-        //numbers[10] = 5; // exception hatası oluşur. Çünkü hafızada numbers için 10. indeks yok
+        // numbers[1] = 1;
+        // //numbers[10] = 5; // exception hatası oluşur. Çünkü hafızada numbers için 10. indeks yok
 
-        System.out.println(numbers);                    // numbers dizisinin adresi basılır
-        System.out.println(Arrays.toString(numbers));   // numbers dizisinin içeriği basılır
+        // System.out.println(numbers);                    // numbers dizisinin adresi basılır
+        // System.out.println(Arrays.toString(numbers));   // numbers dizisinin içeriği basılır
 
-        System.out.println(numbers[1]);
-        System.out.println(numbers2[2]);
+        // System.out.println(numbers[1]);
+        // System.out.println(numbers2[2]);
 
-        System.out.println(numbers2.length);
-        System.out.println(Arrays.toString(numbers2));
-        Arrays.sort(numbers2);
-        System.out.println(Arrays.toString(numbers2));      
+        // System.out.println(numbers2.length);
+        // System.out.println(Arrays.toString(numbers2));
+        // Arrays.sort(numbers2);
+        // System.out.println(Arrays.toString(numbers2));      
 
-        System.out.println(Arrays.toString(names));     // names dizisinin içeriği basılır
+        // System.out.println(Arrays.toString(names));     // names dizisinin içeriği basılır
 
-        // [rows][columns] >> 2, 5
-        int[][] numbers3 = { 
-            {2, 3, 4, 1, 5},
-            {6, 7, 8, 9, 0}
-        };
+        // // [rows][columns] >> 2, 5
+        // int[][] numbers3 = { 
+        //     {2, 3, 4, 1, 5},
+        //     {6, 7, 8, 9, 0}
+        // };
 
-        //2, 2, 3
-        int[][][] numbers4 = { 
-            { {2, 3, 5}, 
-              {4, 1, 7}
-            },
-            { {7, 8, 3}, 
-              {0, 9, 5}
-            },
-        };
+        // //2, 2, 3
+        // int[][][] numbers4 = { 
+        //     { {2, 3, 5}, 
+        //       {4, 1, 7}
+        //     },
+        //     { {7, 8, 3}, 
+        //       {0, 9, 5}
+        //     },
+        // };
 
-        numbers4[0][1][2] = 6;  // 7'yi 6 olarak değiştirir
-        System.out.println(Arrays.deepToString(numbers3));  // multi dimentional array basmak için
-        System.out.println(Arrays.deepToString(numbers4)); 
+        // numbers4[0][1][2] = 6;  // 7'yi 6 olarak değiştirir
+        // System.out.println(Arrays.deepToString(numbers3));  // multi dimentional array basmak için
+        // System.out.println(Arrays.deepToString(numbers4)); 
+        //------------------------------------------------------
+
+        //--- CONSTANTS ----------------------------------------
+        // final float PI = 3.14F; // F koymazsak decimal olarak görür. C'deki "const" ifadesi yerine burada "final" kullanılıyor.
+        // // PI = 1; dersek derleme hatası alırız.
+        // // Ayrıca final değişkenlerin tüm harfleri büyük yazılır.
+
+        // Scanner keyboardInput = new Scanner(System.in); // klavyeden veri girişi tanımlaması
+        // System.out.println("Yaricapi giriniz: ");
+        // float radius = keyboardInput.nextFloat();       // 2,5 olarak virgülle giriş yapılması lazım
+        // System.out.printf("Yaricap degeri: %.2f birim girildi.%n", radius);
+        // keyboardInput.close();
+
+        // float area = (float)(radius*radius) * PI;
+        // NumberFormat formatter = new DecimalFormat("###,###.###");   // number formatting
+        // System.out.printf("Dairenin hesaplanan alani: %.3f birim karedir.%n", area);
+        // System.out.printf("Dairenin hesaplanan alani: %s birim karedir.%n", formatter.format(area));  
+        //------------------------------------------------------
+
+        //--- ARITHMETIC EXPRESSIONS ---------------------------
+        // float result;
+        // float result2;
+        // double result3;
+
+        // result = (float)(10/3);
+        // System.out.println(result); 
+        // // 3.0 değerini basar. result, "double" tipinden olsa da tür dönüşümünde sadece sonucu cast etmek
+        // // yeterli değil. işleme girecek sayıları tür dönüşümüne (cast) sokmak gerekiyor.
+
+        // result2 = (float)10 / (float)3;
+        // System.out.println(result2); // 3.3333333 değerini basar
+        // result3 = (double)10 / (double)3;
+        // System.out.println(result3); // 3.3333333333333335 değerini basar
+     
+        // int x = 1;      // x = 1 olarak tanımlandı
+        // int y = x++;    // y = x (1) olarak tanımlandı ve x bir arttırıldı yani 2 oldu (postfix ++)
+        // int z = ++x;    // z = x + 1 olarak tanımlandı yani 3 oldu bu arada x de 3 oldu (prefix ++)
+        // x -= 3;         // C'deki yazım tarzı burada da var. x = x - 3 olarak atanır.
+        // System.out.printf("x: %d, y: %d, z: %d%n", x, y, z); // terminale 0, 1, 3 değerleri basılır.
+
+        // int i = 10 + 3 * 2;     // önce çarpma yapılacak sonra toplama: 16
+        // int j = (10 + 3) * 2;   // önce parantez içi yapılacak sonra çarpma: 26
+        // // Öncelik sırası: (), * /, + -
+        // System.out.printf("i: %d, j: %d%n", i, j);
+
+        // // Implicit casting (automatic casting/converting)
+        // // byte > short > int > long > float > double şeklinde otomatik dönüştürülür. Sıkıntı çıkmaz.
+        // short k = 1;        // short 2 byte
+        // int m = k + 2;      // int 4 byte. 4 byte içine 2 byte rahatça sığacağı için sorun yok.
+        // //short n = m * 10;   // Tam tersi durumda type missmatch uyarısı verecektir.
+        // System.out.println(m);
+
+        // double o = 1.1;
+        // double p = o + 2;   // int 2 değeri 2.0 şeklinde otomatik olarak dönüştürülür ve işlem yapılır.
+        // System.out.println(p); 
+
+        // // Explicit casting
+        // int r = (int)o + 2;     // o = 1 olarak casting ediliyor.
+        // System.out.println(r);
+
+        // String s = "1";
+        // String t = "1.25";
+
+        // int q = Integer.parseInt(s) + 1;    // Short.parseShort(), Float.parseFloat(), ...vs de var.
+        // float w = Float.parseFloat(t) + 1;
+        // System.out.printf("q: %d, w: %f%n", q, w);
+        //------------------------------------------------------
+
+
+        //--- MATHEMATICAL OPERATIONS --------------------------
+        // int result = Math.round(1.345F);
+        // System.out.println(result);      // 1 değeri basılır
+
+        // double result2 = Math.ceil(1.045F);     // verilen sayıdan büyük en küçük sayıyı döner
+        // int result3 = (int)Math.floor(1.345F);   // verilen sayıdan küçük en küçük sayıyı döner
+        // System.out.printf("result2: %f, result3: %d%n", result2, result3);
+
+        // System.out.printf("En buyuk: %f,%n", Math.max(1.2684, 1.3458)); // verilen sayılardan büyüğünü döner
+        // System.out.printf("En kucuk: %f,%n", Math.min(1.2684, 1.3458)); // verilen sayılardan küçüğünü döner
+        
+        // final float coeff = 2.45364F;   // 0- 100 aralığında istersek bunu 100 yapmak yeterli
+        // System.out.printf("Rasgele Sayi: %f,%n",Math.random() * coeff); // random metodu 0-1 aralığında rasgele sayı üretir 
+        //------------------------------------------------------
+
+
+        //--- NUMBER FORMATTING --------------------------------
+        // NumberFormat formatter = new DecimalFormat("$###,###.##");   // number formatting
+
+        // Scanner keyboardInput = new Scanner(System.in); // klavyeden veri girişi tanımlaması
+        // System.out.println("Ucreti giriniz: ");
+        // long price = keyboardInput.nextLong(); 
+        // keyboardInput.close();
+
+        // System.out.printf("Girilen deger: %s%n", formatter.format(price));  
+
+
+        // NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.UK);
+        // String result = currency.format(1234567.891);   
+        // // para birimine göre format oluşturur. Konum İngiltere seçildi. £ gösterilecek 
+        // // Hindistan bile var ama Türkiye yok !!!
+        // System.out.println(result);
+
+        // NumberFormat percent = NumberFormat.getPercentInstance();
+        // String result2 = percent.format(0.2335);    
+        // //9700 için %970.000, 85 için %8.5, 850 için %85.000, 0.2335 için %23 basılır
+        // System.out.println(result2);
+        // System.out.println(NumberFormat.getPercentInstance().format(0.1));  
+        // // direkt bu şekilde de olur: %10 Buna method chaining deniyor...
         //------------------------------------------------------
     }
 
